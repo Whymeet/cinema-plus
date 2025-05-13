82fb2ae10127e8aceea464ac63ab49e20c4076b6
Whymet <koboc777.mail@gmail.com>
2025-05-13 14:26:39 +0400

    РАБОТАЕТ

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true, // Современная опция
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
