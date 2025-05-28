ebaeb482fd02279af68a8ab977ea2861572d18f8
alina <alina.chubar@mail.ru>
2025-05-28 22:27:38 +0400

    изменение цвета календарей у админа

--- client/src/theme/index.js
+++ client/src/theme/index.js
@@ -3,11 +3,13 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import palette from './palette';
 import paletteDark from './paletteDark';
 import typography from './typography';
+import overrides from './overrides';
 
 // read more at https://material-ui.com/customization/themes
 const theme = createMuiTheme({
   palette: palette,
   typography,
+  overrides,
   zIndex: {
     appBar: 1200,
     drawer: 1100
---
+++ client/src/theme/overrides.js
@@ -0,0 +1,35 @@
+export default {
+  MuiPickersCalendar: {
+    root: {
+      '& .MuiIconButton-root': {
+        color: '#ffffff'
+      },
+      '& .MuiPickersCalendarHeader-switchHeader': {
+        color: '#ffffff'
+      },
+      '& .MuiPickersCalendarHeader-iconButton': {
+        color: '#ffffff'
+      },
+      '& .MuiPickersCalendarHeader-transitionContainer': {
+        color: '#ffffff'
+      },
+      '& .MuiPickersCalendarHeader-dayLabel': {
+        color: '#ffffff'
+      }
+    }
+  },
+  MuiPickersModal: {
+    dialogRoot: {
+      '& .MuiPickersCalendarHeader-iconButton': {
+        color: '#ffffff'
+      }
+    }
+  },
+  MuiInputAdornment: {
+    root: {
+      '& .MuiIconButton-root': {
+        color: '#ffffff'
+      }
+    }
+  }
+}; 
