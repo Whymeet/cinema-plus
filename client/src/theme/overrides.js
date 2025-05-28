72548dfdcf4b205e90591f883864f413e377a22a
alina <alina.chubar@mail.ru>
2025-05-28 23:40:26 +0400

    изменение стилей администратора

--- client/src/theme/overrides.js
+++ client/src/theme/overrides.js
@@ -31,5 +31,49 @@ export default {
         color: '#ffffff'
       }
     }
+  },
+  
+  MuiOutlinedInput: {
+    root: {
+      backgroundColor: '#0767DB',
+      '&:hover': {
+        backgroundColor: '#0767DB'
+      },
+      '&.Mui-focused': {
+        backgroundColor: '#0767DB'
+      },
+      '& input': {
+        fontSize: '1.1rem'
+      }
+    }
+  },
+  MuiSelect: {
+    outlined: {
+      backgroundColor: '#0767DB',
+      fontSize: '1.1rem'
+    }
+  },
+  MuiInputBase: {
+    root: {
+      '&.MuiOutlinedInput-root': {
+        backgroundColor: '#0767DB',
+        fontSize: '1.1rem'
+      }
+    }
+  },
+  MuiInputLabel: {
+    root: {
+      color: '#ffffff',
+      '&.Mui-focused': {
+        color: '#ffffff'
+      }
+    },
+    outlined: {
+      fontSize: '1.1rem',
+      color: '#ffffff',
+      '&.Mui-focused': {
+        color: '#ffffff'
+      }
+    }
   }
 }; 
